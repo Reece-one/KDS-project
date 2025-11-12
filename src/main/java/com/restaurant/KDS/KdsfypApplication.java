efa1ca601973d4f8c59b684b5092deb0359476bc
Reece N <220087944@aston.ac.uk>
2025-11-12 16:51:55 +0000

    Initial commit

---
+++ src/main/java/com/restaurant/KDS/KdsfypApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.KDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdsfypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdsfypApplication.class, args);
+	}
+
+}
